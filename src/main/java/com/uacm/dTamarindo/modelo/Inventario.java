6f9096abaf5ce3b314dfe44d80680009eceeb7c1
IngridCasales96 <56126097+IngridCasales96@users.noreply.github.com>
2020-11-25 04:05:19 -0600

    dominio inventario

--- src/main/java/com/uacm/dTamarindo/modelo/Inventario.java
+++ src/main/java/com/uacm/dTamarindo/modelo/Inventario.java
@@ -1,6 +1,46 @@
 package com.uacm.dTamarindo.modelo;
 
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Inventario {
 	
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "invoice")
+	private List<Producto> productos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Producto> getLines() {
+		return productos;
+	}
+
+	public void setLines(List<Producto> lines) {
+		this.productos = lines;
+	}
+
+
 }
